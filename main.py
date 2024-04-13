73c0f665f97cdd2ff18fd12c2bcb478f71b458a9
Danilo Félix Torres <94990131+daniloftx@users.noreply.github.com>
2024-04-13 18:27:38 +0000

    comment handling

--- main.py
+++ main.py
@@ -4,30 +4,24 @@ import labels
 
 lines = []
 
-basic_syntax = re.compile("^((\s)*\/{2})|^\s*(\w)+\:?\s*((\-?\w)*)?($|(\s*\/{2}))")
-comment_syntax = re.compile("^(\/{2})")
+basic_syntax = re.compile("^\s*(\w+):?\s*(\-?\w*)?($|\s*//)")
 label_syntax = re.compile("^(\s*(\w)+)\:")
 blank_line = re.compile("^\s*$")
 split_lines = [] 
-#print(hBox.match(s))
-#print(re.split("\s+", s.strip()))
 
-#for i in re.split("\s+", s.strip()):
-#    print(i)
 file = open("teste.txt", "r")
 
 for line in file:
-    if blank_line.match(line) is None:
-        if(basic_syntax.match(line)):
-            split_line = re.split("\s+", line.strip())
-            #print(split_line)
-            if(comment_syntax.match(split_line[0]) is None):
-                if(label_syntax.match(split_line[0]) is None):
-                    lines.append(line)
-                    split_lines.append(split_line)
+    line_nocomment = re.sub("\/{2}.*", "", line)
+    if blank_line.match(line_nocomment) is None:
+        if(basic_syntax.match(line_nocomment)):
+            split_line = re.split("\s+", line_nocomment.strip())
+            if(label_syntax.match(split_line[0]) is None):
+                lines.append(line_nocomment)
+                split_lines.append(split_line)
                     #print(line)
-                else:
-                    labels.labels[split_line[0]] = len(lines)
+            else:
+                labels.labels[split_line[0]] = len(lines)
         else:
             print("syntax error")
 
