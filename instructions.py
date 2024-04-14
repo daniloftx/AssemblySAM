b2642ee7d10cd3c396ffbd30921e0febd73e58e8
Gabriel <gabrielliima.dev@gmail.com>
2024-04-14 18:34:12 -0300

    implement pc and halt, change error messages and fix bugs

--- instructions.py
+++ instructions.py
@@ -5,60 +5,54 @@ def add(line, control):
         print(f'{line[0]} error')
         control.halt = 1
     else:
-        if len(control.stack) < 2:
-            print(f'{line[0]} error')
-            control.halt = 1
-        else:
-            control.stack.append(control.stack.pop() + control.stack.pop())
-            control.sp -= 1
+        control.stack.append(control.pop() + control.pop())
+
+        control.sp -= 1
 
 def sub(line, control):
     if len(line) > 1:
         print(f'{line[0]} error')
         control.halt = 1
     else:
-        if len(control.stack) < 2:
-            print(f'{line[0]} error')
-            control.halt = 1
-        else:
-          control.stack.append(control.stack.pop() - control.stack.pop())
-          control.sp -= 1
+        vt = control.pop()
+        vb = control.pop()
+
+        control.stack.append(vb - vt)
+
+        control.sp -= 1
         
 def times(line, control):
     if len(line) > 1:
         print(f'{line[0]} error')
         control.halt = 1
     else:
-        if len(control.stack) < 2:
-            print(f'{line[0]} error')
-            control.halt = 1
-        else:
-          control.stack.append(control.stack.pop() * control.stack.pop())
-          control.sp -= 1
+        control.stack.append(control.pop() * control.pop())
+
+        control.sp -= 1
 
 def div(line, control):
     if len(line) > 1:
         print(f'{line[0]} error')
         control.halt = 1
     else:
-        if len(control.stack) < 2:
-            print(f'{line[0]} error')
-            control.halt = 1
-        else:
-          control.stack.append(control.stack.pop() // control.stack.pop())
-          control.sp -= 1
+        vt = control.pop()
+        vb = control.pop()
+
+        control.stack.append(vb // vt)
+
+        control.sp -= 1
     
 def mod(line, control):
     if len(line) > 1:
         print(f'{line[0]} error')
         control.halt = 1
     else:
-        if len(control.stack) < 2:
-            print(f'{line[0]} error')
-            control.halt = 1
-        else:
-          control.stack.append(control.stack.pop() % control.stack.pop())
-          control.sp -= 1
+        vt = control.pop()
+        vb = control.pop()
+
+        control.stack.append(vb % vt)
+
+        control.sp -= 1
 
 #Comparison
 def cmp(line, control):
@@ -66,113 +60,92 @@ def cmp(line, control):
         print(f'{line[0]} error')
         control.halt = 1
     else:
-        if len(control.stack) < 2:
-            print(f'{line[0]} error')
-            control.halt = 1
-        else:
-            vt = control.stack.pop()
-            vb = control.stack.pop()
-            if vt > vb:
-                control.stack.append(1)
-            elif vt == vb:
-                control.stack.append(0)
-            else:
-                control.stack.append(-1)
-            control.sp -= 1
-            
+        vt = control.pop()
+        vb = control.pop()
 
+        if vt > vb:
+            control.stack.append(1)
+        elif vt == vb:
+            control.stack.append(0)
+        else:
+            control.stack.append(-1)
+
+        control.sp -= 1
+            
 def greater(line, control):
     if len(line) > 1:
         print(f'{line[0]} error')
         control.halt = 1
     else:
-        if len(control.stack) < 2:
-            print(f'{line[0]} error')
-            control.halt = 1
+        vt = control.pop()
+        vb = control.pop()
+
+        if vb > vt:
+            control.stack.append(1)
         else:
-            vt = control.stack.pop()
-            vb = control.stack.pop()
-            if vt > vb:
-                control.stack.append(1)
-            else:
-                control.stack.append(0)
-            control.sp -= 1
+            control.stack.append(0)
+
+        control.sp -= 1
 
 def less(line, control):
     if len(line) > 1:
         print(f'{line[0]} error')
         control.halt = 1
     else:
-        if len(control.stack) < 2:
-            print(f'{line[0]} error')
-            control.halt = 1
+        vt = control.pop()
+        vb = control.pop()
+
+        if vb < vt:
+            control.stack.append(1)
         else:
-            vt = control.stack.pop()
-            vb = control.stack.pop()
-            if vt < vb:
-                control.stack.append(1)
-            else:
-                control.stack.append(0)
-            control.sp -= 1
+            control.stack.append(0)
+
+        control.sp -= 1
 
 def equal(line, control):
     if len(line) > 1:
         print(f'{line[0]} error')
         control.halt = 1
     else:
-        if len(control.stack) < 2:
-            print(f'{line[0]} error')
-            control.halt = 1
+        vt = control.pop()
+        vb = control.pop()
+
+        if vt == vb:
+            control.stack.append(1)
         else:
-            vt = control.stack.pop()
-            vb = control.stack.pop()
-            if vt == vb:
-                control.stack.append(1)
-            else:
-                control.stack.append(0)
-            control.sp -= 1
+            control.stack.append(0)
+
+        control.sp -= 1
 
 def isnil(line, control):
     if len(line) > 1:
         print(f'{line[0]} error')
         control.halt = 1
     else:
-        if len(control.stack) < 1:
-            print(f'{line[0]} error')
-            control.halt = 1
+        if control.pop() == 0:
+            control.stack.append(1)
         else:
-            if control.stack.pop() == 0:
-                control.stack.append(1)
-            else:
-                control.stack.append(0)
+            control.stack.append(0)
             
 def ispos(line, control):
     if len(line) > 1:
         print(f'{line[0]} error')
         control.halt = 1
     else:
-        if len(control.stack) < 1:
-            print(f'{line[0]} error')
-            control.halt = 1
+        if control.pop() > 0:
+            control.stack.append(1)
         else:
-            if control.stack.pop() > 0:
-                control.stack.append(1)
-            else:
-                control.stack.append(0)
+            control.stack.append(0)
 
 def isneg(line, control):
     if len(line) > 1:
         print(f'{line[0]} error')
         control.halt = 1
     else:
-        if len(control.stack) < 1:
-            print(f'{line[0]} error')
-            control.halt = 1
+        if control.pop() < 0:
+            control.stack.append(1)
         else:
-            if control.stack.pop() < 0:
-                control.stack.append(1)
-            else:
-                control.stack.append(0)
+            control.stack.append(0)
 
 #jumps
 def jump(line, control):
@@ -181,53 +154,73 @@ def jump(line, control):
     else:
         n = line[1].lstrip('-')
         if n.isdigit():
+            if int(n) > control.length - 1:
+                print(f'Processor Error: Invalid index at instruction {control.pc}.')
+                exit(1)
+
             if line[1][0] == '-':
                 print(f'{line[0]} error')
                 control.halt = 1
             else:
-                control.pc = int(n)
+                # Subtract 1 because pc will be incremented after this function execution
+                control.pc = int(n) - 1
         else:
             if n+':' in control.labels:
-                control.pc = control.labels[n+':']
+                # Subtract 1 for same reason above
+                control.pc = control.labels[n+':'] - 1
             else: 
-                print(f'Adress {n} not found')
+                print(f'Processor Error: Unresolved reference "{n}" at instruction {control.pc}.')
+                exit(1)
 
 def jumpc(line, control):
     if len(line) < 2:
         print(f'{line[0]} error')
     else:
-        if(control.stack[control.sp - 1] != 0):
+        vt = control.pop()
+
+        if(vt != 0):
             n = line[1].lstrip('-')
+
             if n.isdigit():
                 if line[1][0] == '-':
                     print(f'{line[0]} error')
                     control.halt = 1
                 else:              
-                    control.pc = int(n)
+                    # Subtract 1 because pc will be incremented after this function execution
+                    control.pc = int(n) - 1
             else:
                 if n+':' in control.labels:     
-                    control.pc = control.labels[n+':']
+                    # Subtract 1 for same reason above
+                    control.pc = control.labels[n+':'] - 1
                 else: 
-                    print(f'Adress {n} not found')
+                    print(f'Processor Error: Unresolved reference "{n}" at instruction {control.pc}.')
+                    exit(1)
 
 def jsr(line, control):
     if len(line) < 2:
         print(f'{line[0]} error')
     else:
         n = line[1].lstrip('-')
+
         if n.isdigit():
             if line[1][0] == '-':
                 print(f'{line[0]} error')
                 control.halt = 1
             else:
                 control.stack.append(control.pc + 1)
-                control.pc = int(n)
+
+                # Subtract 1 because pc will be incremented after this function execution
+                control.pc = int(n) - 1
         else:
             if n+':' in control.labels:
                 control.stack.append(control.pc + 1)
-                control.pc = control.labels[n+':']
+
+                # Subtract 1 for same reason above
+                control.pc = control.labels[n+':'] - 1
             else: 
-                print(f'Adress {n} not found')
+                print(f'Processor Error: Unresolved reference "{n}" at instruction {control.pc}.')
+                exit(1)
+
         control.sp += 1
 
 def skip(line, control):
